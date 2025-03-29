8c71dacc5afde1ba8bbb77402a53d9d48daba40c
Vinod Rahal <smvinodrahal@gmail.com>
2025-03-29 22:50:47 +0530

    Complete API with Join query

--- TechWeb/TechWeb/Controllers/ProductImagesController.cs
+++ TechWeb/TechWeb/Controllers/ProductImagesController.cs
@@ -42,6 +42,7 @@ public class ProductImagesController : Controller
         var Product_Images = await _context.P_Images.Where(p => p.ProductID == productID)
             .Select(p => new Product_Image
             {
+                ImageId = p.ImageId,
                 ProductID = p.ProductID,
                 ImageName = string.Format("{0}://{1}{2}/Images/{3}/{4}",Request.Scheme,Request.Host,Request.PathBase,p.ImageName,p.ImageName)
             }
@@ -55,4 +56,30 @@ public class ProductImagesController : Controller
         
         return Ok(Product_Images);
     }
+    
+    [HttpGet("GetProductsWithImages")]
+    public async Task<ActionResult<IEnumerable<object>>> GetProductsWithImages()
+    {
+        var result = await (from p in _context.Products
+                join i in _context.P_Images
+                    on p.Product_ID equals i.ProductID // Ensure correct key match
+                select new
+                {
+                    ProductID = p.Product_ID,
+                    ProductTitle = p.Title,
+                    ProductPrice = p.Price,
+                    ProductOffer = p.Offer,
+                    ProductReview = p.Review,
+                    ProductStock = p.Stock,
+                    ImageURL = string.Format("{0}://{1}{2}/Images/{3}/{4}",Request.Scheme,Request.Host,Request.PathBase,i.ImageName,i.ImageName)
+                })
+            .FirstOrDefaultAsync();
+
+        return Ok(result);
+    }
+
+
+    
+
+    
 }
